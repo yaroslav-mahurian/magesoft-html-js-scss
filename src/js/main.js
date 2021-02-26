3c9bc69ee70fc24865f695da3cae4ea2b9bd41ea
Yaroslav <maguryan.mail@gmail.com>
2021-02-26 02:17:15 +0200

    complete html blocks

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,106 @@
+import lax from 'lax.js';
+
+
+
+window.onload = function () {
+    
+    const lineCoopRound = document.querySelector('#lineCoopRound');
+    
+    const coopAnimElementsArr = Array.from(document.querySelectorAll('[data-coop-anim-elem]'));
+
+    window.addEventListener("scroll", checkActiveCoopEl);
+
+    function checkActiveCoopEl() {
+        const lineCoopRoundOffset = offset(lineCoopRound).top; 
+
+        coopAnimElementsArr.forEach((el) => {
+            if(lineCoopRoundOffset >= offset(el).top) {
+
+                if (el.className === "coop-list__el") {
+                    el.classList.add('coop-list__el--active');
+                } else {
+                    el.classList.add('btn--coop-active');
+                }
+
+            } else {
+                
+                el.classList.remove('coop-list__el--active');
+                el.classList.remove('btn--coop-active');
+            }
+        });
+    }
+
+    lax.init();
+
+    // Add a driver that we use to control our animations
+    lax.addDriver('scrollY', function () {
+        return window.scrollY;
+    });
+
+    // Add animation bindings to elements
+    lax.addElements('#lineCoopRound', {
+        scrollY: {
+        translateY: [
+            [
+                "elCenterY", 
+                "elCenterY + 239", 
+                "elCenterY + 300", 
+                "elCenterY + 445",
+                "elCenterY + 510",
+                "elCenterY + 650",
+                "elCenterY + 720",
+                "elCenterY + 856",
+                "elCenterY + 926",
+                "elCenterY + 1063",
+                "elCenterY + 1133",
+                "elCenterY + 1222",
+            ],
+            [
+                0, 
+                239,  
+                239, 
+                445,
+                445,
+                650,
+                650,
+                856,
+                856,
+                1063,
+                1063,
+                1222,
+            ],
+        ],
+        translateX: [ 
+            [
+                "elCenterY + 239", 
+                "elCenterY + 300", 
+                "elCenterY + 445", 
+                "elCenterY + 510",
+                "elCenterY + 650",
+                "elCenterY + 720",
+                "elCenterY + 856",
+                "elCenterY + 926",
+                "elCenterY + 1063",
+                "elCenterY + 1133",
+            ],
+            [
+                0, 
+                337, 
+                337, 
+                -429,
+                -429,
+                344,
+                344,
+                -576,
+                -576,
+                299,
+            ],
+        ],
+        }
+    });
+};
+
+
 /* --------------------------------- SCROLL ACTIONS --------------------------------- */ 
 
 
@@ -108,23 +211,54 @@ function runScrollScript() {
 
 setTimeout(runScrollScript, 200);
 
+/* window.addEventListener('resize', function() {
+    setTimeout(runScrollScript, 200);
+}, true); */
+
+
 /* --------------------------------- MENU --------------------------------- */ 
 
 const burgerMenu = document.querySelector('#burgerMenu');
 const burgerBtn = document.querySelector('#burgerBtn');
 const menuWrapper = document.querySelector('#menuWrapper');
+const contactWrapper = document.querySelector('#contactWrapper');
+const oneProjectWrapper = document.querySelector('#oneProjectWrapper');
+const oneNewsWrapper = document.querySelector('#oneProjectWrapper');
 const menuWrapperLogo = document.querySelector('#menuWrapperLogo');
 const closeMenuBtns = document.querySelectorAll("[data-action-close-menu]");
-
+const closeContactBtn = document.querySelector("[data-action-close-contact]");
+const showContactWrapperLinks = document.querySelectorAll("[data-link-contact]");
 
 burgerMenu.addEventListener("click", () => {
     burgerBtn.classList.toggle('is-active');
-    menuWrapper.classList.toggle('menu-wrapper--active');
-    if (menuWrapper.hasAttribute("data-status-active")) {
-        menuWrapper.removeAttribute("data-status-active");
-    } else {
-        menuWrapper.setAttribute("data-status-active", "");
-    }
+
+        if(contactWrapper) {
+            if (contactWrapper.hasAttribute("data-status-active")) {
+                contactWrapper.classList.remove('menu-wrapper--active');
+        
+                if (contactWrapper.hasAttribute("data-status-active")) {
+                    contactWrapper.removeAttribute("data-status-active");
+                }
+        
+            } else {
+                if (menuWrapper.hasAttribute("data-status-active")) {
+                    menuWrapper.removeAttribute("data-status-active");
+                } else {
+                    menuWrapper.setAttribute("data-status-active", "");
+                }
+                
+                menuWrapper.classList.toggle('menu-wrapper--active');
+            }
+
+        } else {
+            if (menuWrapper.hasAttribute("data-status-active")) {
+                menuWrapper.removeAttribute("data-status-active");
+            } else {
+                menuWrapper.setAttribute("data-status-active", "");
+            }
+            
+            menuWrapper.classList.toggle('menu-wrapper--active');
+        }
 
     if(menuLogo.dataset.status === "active") {  
         menuWrapperLogo.style.opacity = "0";
@@ -143,6 +277,29 @@ Array.from(closeMenuBtns).forEach((btn) => {
         }        
     });
 });
+
+if (closeContactBtn) {
+    closeContactBtn.addEventListener("click", () => {
+        contactWrapper.classList.remove('menu-wrapper--active');
+        burgerBtn.classList.toggle('is-active');
+        contactWrapper.removeAttribute("data-status-active");
+    });
+}
+
+
+Array.from(showContactWrapperLinks).forEach((link) => {
+    link.addEventListener("click", (e)=> {
+        e.preventDefault();
+        burgerBtn.classList.add('is-active');
+        contactWrapper.classList.add('menu-wrapper--active');
+        if (contactWrapper.hasAttribute("data-status-active")) {
+            contactWrapper.removeAttribute("data-status-active");
+        } else {
+            contactWrapper.setAttribute("data-status-active", "");
+        }
+    });
+});
+
 
 
 /* --------------------------------- Get offset func --------------------------------- */
@@ -179,7 +336,7 @@ window.addEventListener('resize', () => {
 
 /* -------------------- Smooth scroll for anchors script ------------------------ */
 /*   */
-const anchors = document.querySelectorAll('a[href*="#"]');
+const anchors = document.querySelectorAll('[data-smooth-scroll]');
 Array.from(anchors).forEach((anchor) => {
 	anchor.addEventListener("click", function(e) {
 		e.preventDefault();
@@ -190,3 +347,72 @@ Array.from(anchors).forEach((anchor) => {
 		});
 	});
 });
+
+/* ---------------------------------- SWIPER ----------------------------------- */
+
+import SwiperCore, { Pagination, Scrollbar } from 'swiper/core';
+        // configure Swiper to use modules
+        SwiperCore.use([Pagination, Scrollbar]);
+
+        // init Swiper:
+    const teamSlider = new SwiperCore('#teamSlider', {
+            // Optional parameters
+        wrapperClass: 'team-slider-wrapper', 
+        slideClass: 'team-slide',   
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 3,	
+        spaceBetween: 90,
+
+        // Responsive breakpoints
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 30
+            },
+            768: {
+                slidesPerView: 1,
+                spaceBetween: 50
+            },
+            1000: {
+                slidesPerView: 3,
+                spaceBetween: 2
+            },
+            1280: {
+                slidesPerView: 3,
+                spaceBetween: 90
+            }
+        },
+        // If we need pagination
+        pagination: {
+            el: '.team-slider-pagination',
+            bulletClass: 'team-slider-pagination-el',
+            bulletActiveClass: 'team-slider-pagination-el-active',
+            clickable: true,
+        },
+    });
+
+    const clientsSlider = new SwiperCore('#clientsSlider', {
+        wrapperClass: 'clients-slider-wrapper', 
+        slideClass: 'clients-slide', 
+        direction: 'horizontal',
+        loop: false,
+
+        breakpoints: {
+            320: {
+                slidesPerView: 3,
+            },
+            480: {
+                slidesPerView: 3,
+            },
+            768: {
+                slidesPerView: 4,
+            },
+            1000: {
+                slidesPerView: 4,
+            },
+            1280: {
+                slidesPerView: 6,
+            }
+        },
+    });
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         babelpolyfill: '@babel/polyfill',
         index: './src/js/main.js',
     },
-    mode: 'production',
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
